253a04f64689075957d7d68b4d243dbc679c6f1f
Liz Burke <liz.burke.chi@gmail.com>
2021-01-13 19:10:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 def using_concat(array_1, array_2)
-array_1.concat(array_2)
+  array_1.concat(array_2)
 end
 
 def using_insert(array, element)
-puts array[4].insert(element)
+  array.insert(3, element)
 end
